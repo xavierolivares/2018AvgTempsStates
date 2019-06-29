8632c2d8c22d931ef1696e82b72a818d9035115b
Xavier Olivares <xm.olivares@gmail.com>
2019-06-29 17:59:12 -0400

    NV to WY

---
+++ script.js
@@ -0,0 +1,12 @@
+const data = require("./completed/ND");
+
+const avg = thing => {
+  let sum = 0;
+  for (let i = 0; i < thing.length; i++) {
+    sum += thing[i].TAVG;
+  }
+  let average = sum / thing.length;
+  return average.toFixed(2);
+};
+
+console.log("average temp of NJ in 2018:", avg(data));
